88b358ee65d1201f1903d762e70e094b34154836
thienth32 <thienth32@gmail.com>
2019-03-27 13:32:39 +0700

    update

--- lesson3/routes/index.js
+++ lesson3/routes/index.js
@@ -9,4 +9,27 @@ router.get('/', function(req, res, next) {
   })
 });
 
+router.get('/cates/add', function(req, res, next){
+  res.render('category/add-form');
+});
+
+router.post('/cates/save-add', function(req, res, next){
+  // thu thập dữ liệu từ form
+  let {name, image, description} = req.body;
+  
+  // tạo ra đối tượng mới dạng Category
+  let model = new Category();
+
+  // Cập nhật lại thông tin đối tượng vừa tạo với dữ liệu thu thập được từ form
+  model.name = name;
+  model.image = image;
+  model.description = description;
+
+  // Lưu đối tượng với csdl
+  model.save();
+
+  // điều hướng website về danh sách danh mục
+  res.redirect('/');
+});
+
 module.exports = router;
