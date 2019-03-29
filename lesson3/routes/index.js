22a030d7aabefa0ccc62d9e341c5b07a261b9a0a
thienth32 <thienth32@gmail.com>
2019-03-29 12:23:59 +0700

    upload image

--- lesson3/routes/index.js
+++ lesson3/routes/index.js
@@ -1,6 +1,20 @@
 var express = require('express');
 var router = express.Router();
+var multer = require('multer');
+var storage = multer.diskStorage({
+  // nơi lưu trữ ảnh đc upload lên server
+  destination: function(req, file, cb){
+    cb(null, './public/uploads');
+  },
+  // quy định tên file đc upload lên
+  filename: function(req, file, cb){
+    cb(null, file.originalname);
+  }
+});
+var upload = multer({storage: storage});
+
 var Category = require('../models/category');
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -13,23 +27,24 @@ router.get('/cates/add', function(req, res, next){
   res.render('category/add-form');
 });
 
-router.post('/cates/save-add', function(req, res, next){
+router.post('/cates/save-add', upload.single('image'),function(req, res, next){
   // thu thập dữ liệu từ form
-  let {name, image, description} = req.body;
+  let {name, description} = req.body;
+  res.json(req.file);
   
   // tạo ra đối tượng mới dạng Category
-  let model = new Category();
+  // let model = new Category();
 
-  // Cập nhật lại thông tin đối tượng vừa tạo với dữ liệu thu thập được từ form
-  model.name = name;
-  model.image = image;
-  model.description = description;
+  // // Cập nhật lại thông tin đối tượng vừa tạo với dữ liệu thu thập được từ form
+  // model.name = name;
+  // model.image = image;
+  // model.description = description;
 
-  // Lưu đối tượng với csdl
-  model.save();
+  // // Lưu đối tượng với csdl
+  // model.save();
 
-  // điều hướng website về danh sách danh mục
-  res.redirect('/');
+  // // điều hướng website về danh sách danh mục
+  // res.redirect('/');
 });
 
 router.get('/cates/remove/:cateId', function(req, res, next){
